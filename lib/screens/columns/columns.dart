3bd5d4a9644ae926eae7a24330f7dad3ece21a54
PaulSong213 <songaliajohnpaul@gmail.com>
2022-02-03 21:19:06 +0800

    Add column lay out and image via asset

---
+++ lib/screens/columns/columns.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'local_widgets/image_banner.dart';
+import 'local_widgets/text_section.dart';
+
+class Columns extends StatefulWidget {
+  const Columns({Key? key}) : super(key: key);
+
+  @override
+  _ColumnsState createState() => _ColumnsState();
+}
+
+class _ColumnsState extends State<Columns> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text('Columns Section'),
+        elevation: 0,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          ImageBanner("assets/images/banner.jpeg"),
+          TextSection(Colors.green),
+          TextSection(Colors.blue),
+          TextSection(Colors.red),
+        ],
+      ),
+    );
+  }
+}
