0e46d641e6873c62f7eb0f61c4d69f0032c952f5
PaulSong213 <songaliajohnpaul@gmail.com>
2022-02-13 21:33:02 +0800

    Make card animation like button

--- lib/screens/landing_page/local_widgets/menu_card.dart
+++ lib/screens/landing_page/local_widgets/menu_card.dart
@@ -9,34 +9,65 @@ class MenuCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        //go to the menu section
-      },
-      child: Container(
-        height: 135,
-        width: 135,
-        padding: EdgeInsets.all(10),
-        margin: EdgeInsets.all(10),
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            fit: BoxFit.cover,
-            image: NetworkImage(_imageBannerUrl),
+    return Container(
+      margin: EdgeInsets.all(10),
+      child: GestureDetector(
+        onTap: () {
+          //go to the menu section
+        },
+        child: Container(
+          height: 135,
+          width: 135,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: NetworkImage(_imageBannerUrl),
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(20)),
           ),
-          borderRadius: BorderRadius.all(Radius.circular(20)),
-        ),
-        child: Text(
-          _title,
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontWeight: FontWeight.w700,
-            fontSize: 18,
-            color: Colors.white,
-            shadows: const <Shadow>[
-              Shadow(
-                offset: Offset(5.0, 2.0),
-                blurRadius: 3.0,
-                color: Colors.black87,
+          child: Stack(
+            children: [
+              Container(
+                padding: EdgeInsets.all(5),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      _title,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 18,
+                        color: Colors.white,
+                        shadows: const <Shadow>[
+                          Shadow(
+                            offset: Offset(5.0, 2.0),
+                            blurRadius: 3.0,
+                            color: Colors.black87,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                top: 0,
+                left: 0,
+                child: ElevatedButton(
+                  onPressed: () => {},
+                  style: ElevatedButton.styleFrom(
+                      elevation: 0,
+                      primary: Colors.black.withOpacity(0.0),
+                      shape: (RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      )) // background
+                      ),
+                  child: SizedBox(
+                    height: 135,
+                    width: 103,
+                  ),
+                ),
               ),
             ],
           ),
